ccb4d01aed7af23b414226e853faf87cab79206f
AhammadSha <118877104+AhammadSha@users.noreply.github.com>
2026-05-13 17:17:04 +0530

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,6 +1,6 @@
 <?php
 
 define('ROOT_PATH', $_SERVER['DOCUMENT_ROOT']);// remove  . '/Localhost/brilliantcomputers.in' in real srever
-define('BASE_URL', 'https://brilliantcomputers.in');
+define('BASE_URL', 'https://brilliantcomputers.in/');
 
 ?>
